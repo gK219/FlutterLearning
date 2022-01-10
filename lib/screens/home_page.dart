f3eff468c6cdc842a578ab32e3b340c9553c204b
gk21 <gmkheros21@gmail.com>
2022-01-11 03:37:46 +0530

    grid view screen

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -12,8 +12,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final int days = 30;
-
-  final String name =  "Flutter learnings";
+  final String name = "Flutter learnings";
 
   @override
   void initState() {
@@ -22,39 +21,78 @@ class _HomePageState extends State<HomePage> {
   }
 
   loadData() async {
-    await Future.delayed(Duration(seconds: 10));
-    final catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    await Future.delayed(Duration(seconds: 4));
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
     final decodeData = jsonDecode(catalogJson);
-    var productData =  decodeData["products"];
+    var productData = decodeData["products"];
     //print(productData);
-     CatalogModel.items = List.from(productData)
-         .map<Item>((item) => Item.fromMap(item))
-         .toList();
-
+    CatalogModel.items =
+        List.from(productData).map<Item>((item) => Item.fromMap(item)).toList();
     setState(() {});
-
   }
 
   @override
   Widget build(BuildContext context) {
     //final dummyList = List.generate(10, (index) => CatalogModel.item[0]);
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Catalog App")
-      ),
+      appBar: AppBar(title: const Text("Catalog App")),
       body: Padding(
         padding: const EdgeInsets.all(10.0),
-        child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty ) ? ListView.builder(
-          itemCount: CatalogModel.items.length,
-          itemBuilder: (context, index) {
-          return ItemWidget(
-            item: CatalogModel.items[index],
-          );
-          },
-        )
-        : Center(
-          child: CircularProgressIndicator(),
-        ),
+        child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
+            ? GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 16,
+                    crossAxisSpacing: 16),
+                itemBuilder: (context, index) {
+                  final item = CatalogModel.items[index];
+                  return Card(
+                    clipBehavior: Clip.antiAlias,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: GridTile(
+                      header: Container(
+                        child: Text(
+                          item.name,
+                          style: const TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                        padding: EdgeInsets.all(12),
+                        decoration: const BoxDecoration(
+                          color: Colors.deepPurple,
+                        ),
+                      ),
+                      child: Image.network(item.image),
+                      footer: Container(
+                        child: Text(
+                          item.price.toString(),
+                          style: const TextStyle(color: Colors.redAccent),
+                          textAlign: TextAlign.right,
+                        ),
+                        padding: const EdgeInsets.all(12),
+                        decoration: const BoxDecoration(
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  );
+                },
+                itemCount: CatalogModel.items.length,
+              )
+            // (
+            //       itemCount: CatalogModel.items.length,
+            //       itemBuilder: (context, index) {
+            //         return ItemWidget(
+            //           item: CatalogModel.items[index],
+            //         );
+            //       },
+            //     )
+            : const Center(
+                child: CircularProgressIndicator(),
+              ),
       ),
       drawer: MyDrawer(),
     );
